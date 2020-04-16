ad7ffc386b43c04ecbb2d32a5f4840fb0b788fed
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-04-16 21:19:06 +0200

    Wypisanie struktury profilu

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Aux from '../../hoc/AuxComponent/AuxComponent';
 
 const profile = props => (
-    <div></div>
+    <Aux>
+        <nav>
+            <div>Zdjęcie profilowe</div>
+            <div>Motto</div>
+            <div>Informacje o użytkowniku</div>
+            <div>Box ze znajomymi</div>
+        </nav>
+        <div>Menu profilowe</div>
+        <div>Moje plany podróży</div>
+        <div>Odwiedzone kraje (mapa)</div>
+        <div>Moje albumy</div>
+        <div>Moje artykuły</div>
+        <div>Obserwowani użytkownicy</div>
+    </Aux>
+
 );
 
 export default profile;
