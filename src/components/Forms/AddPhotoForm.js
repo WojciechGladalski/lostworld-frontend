78882ddf6cb1b0716e484dc8fab4992d7e34450a
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-05-01 19:59:01 +0200

    Dodanie metod do formularza dodawania zdjęcia

--- src/components/Forms/AddPhotoForm.js
+++ src/components/Forms/AddPhotoForm.js
@@ -2,6 +2,62 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Aux from '../../hoc/AuxComponent/AuxComponent'
+import axios from "axios";
+
+const inputObject = React.createRef()
+
+function getTemplate() {
+    return axios({
+        method: 'get',
+        url: 'http://2c36ee71.ngrok.io/photos/upload',
+    })
+        .then(response =>{
+            return response.data
+        })
+        .catch(response => {
+            console.log(response)
+            return null
+        });
+}
+
+function fileHandler(event) {
+    event.preventDefault();
+
+    console.log(inputObject)
+    console.log(inputObject.current.files[0])
+
+    const fileBytes = []
+
+    const reader = new FileReader()
+    reader.onload = function () {
+        let arrayBuffer = reader.result
+        let bytes = new Uint8Array(arrayBuffer);
+        bytes.forEach(item => fileBytes.push(item))
+        //console.log(bytes);
+    }
+
+    reader.readAsArrayBuffer(inputObject.current.files[0])
+
+    getTemplate().then(responseObject => {
+
+        responseObject.data = fileBytes
+        responseObject.fileName = inputObject.current.files[0].name
+        responseObject.fileType = inputObject.current.files[0].type
+        responseObject.size = inputObject.current.files[0].size
+
+        axios({
+            method: 'post',
+            url:"http://2c36ee71.ngrok.io/photos/upload",
+            data: responseObject
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (response) {
+                console.log(response);
+            });
+    })
+};
 
 function MyVerticallyCenteredModal(props) {
     return (
@@ -20,9 +76,9 @@ function MyVerticallyCenteredModal(props) {
                 <div className="single-upload">
                     <h3>Upload Single File</h3>
                     <form id="singleUploadForm" name="singleUploadForm">
-                        <input id="singleFileUploadInput" type="file" name="file" className="file-input" required/>
+                        <input ref={inputObject} id="singleFileUploadInput" type="file" name="file" className="file-input" required/>
                         {/*<button type="submit" className="primary submit-btn">Submit</button>*/}
-                        <Button type="submit" variant="success">Dodaj</Button>
+                        <Button type="submit" variant="success" onClick={fileHandler}>Dodaj</Button>
                     </form>
                     <div className="upload-response">
                         <div id="singleFileUploadError"></div>
