3fe83522f6695459ec1050bd21adc08f3a8b4b83
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-05-03 18:19:59 +0200

    Dodanie formularza dodawania avatara

---
+++ src/components/Forms/AddAvatarForm.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import axios from "axios";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Aux from "../../hoc/AuxComponent/AuxComponent";
+
+const inputObject = React.createRef();
+
+const getTemplate = () => {
+    return axios({
+        method: 'get',
+        url: 'http://2c36ee71.ngrok.io/photos/upload',
+    })
+        .then(response =>{
+            return response.data
+        })
+        .catch(response => {
+            console.log(response);
+            return null
+        });
+};
+
+const fileHandler = (event) => {
+    event.preventDefault();
+
+    console.log(inputObject);
+    console.log(inputObject.current.files[0]);
+
+    const fileBytes = [];
+
+    const reader = new FileReader();
+    reader.onload = function () {
+        let arrayBuffer = reader.result;
+        let bytes = new Uint8Array(arrayBuffer);
+        bytes.forEach(item => fileBytes.push(item))
+    };
+
+    reader.readAsArrayBuffer(inputObject.current.files[0]);
+
+    getTemplate().then(responseObject => {
+
+        responseObject.data = fileBytes;
+        responseObject.fileName = inputObject.current.files[0].name;
+        responseObject.fileType = inputObject.current.files[0].type;
+        responseObject.size = inputObject.current.files[0].size;
+
+        axios({
+            method: 'post',
+            url:"http://2c36ee71.ngrok.io/photos/upload",
+            data: responseObject
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (response) {
+                console.log(response);
+            });
+    })
+};
+
+const MyVerticallyCenteredModal = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Załaduj zdjęcie
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="single-upload">
+                    {/*<h3>Upload Single File</h3>*/}
+                    <form id="singleUploadForm" name="singleUploadForm">
+                        <input ref={inputObject} id="singleFileUploadInput" type="file" name="file" className="file-input" required/>
+                        <Button type="submit" variant="success" onClick={fileHandler}>Dodaj</Button>
+                    </form>
+                    <div className="upload-response">
+                        <div id="singleFileUploadError"></div>
+                        <div id="singleFileUploadSuccess"></div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+const AddAvatarForm = (props) => {
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <Aux>
+            <h3 style={{display: 'inline-block', verticalAlign: 'middle'}}>MOJE ALBUMY</h3>
+            <Button type="button" variant="secondary" onClick={() => setModalShow(true)}>Dodaj</Button>
+
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </Aux>
+    );
+};
+
+export default AddAvatarForm;
