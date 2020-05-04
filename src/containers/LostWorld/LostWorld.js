d1447dac5e40596194ccdb8ae23ad13def1b580c
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-05-04 18:11:34 +0200

    Dodanie nowego modułu na główna stronę

--- src/containers/LostWorld/LostWorld.js
+++ src/containers/LostWorld/LostWorld.js
@@ -2,20 +2,21 @@ import React, {Component} from 'react';
 import classes from './LostWorld.module.css';
 // import backgroundImage from '../../assets/images/background.jpg';
 import MainCarousel from '../../components/UI/Carousel/MainCarousel'
+import PopularPlaces from './PopularPlaces/PopularPlaces';
+import Aux from '../../hoc/AuxComponent/AuxComponent';
 
 class LostWorld extends Component {
     render() {
         return (
-            <div className={classes.LostWorld}>
-                {/*<img src={backgroundImage} alt='landscape'/>*/}
-                <div className={classes.Carousel}>
-                    <MainCarousel/>
+            <Aux>
+                <div className={classes.LostWorld}>
+                    {/*<img src={backgroundImage} alt='landscape'/>*/}
+                    <div className={classes.Carousel}>
+                        <MainCarousel/>
+                    </div>
+                    <PopularPlaces/>
                 </div>
-
-                <div>
-
-                </div>
-            </div>
+            </Aux>
         );
     }
 }
