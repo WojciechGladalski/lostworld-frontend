d1350e196afbb85344847f1ff52c767292bffb21
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-06-06 15:45:19 +0200

    Usunięcie komentarzy

--- src/components/Profile/Following/ProfileFollowingUsers.js
+++ src/components/Profile/Following/ProfileFollowingUsers.js
@@ -2,23 +2,7 @@ import React, {useState, useEffect} from 'react';
 import classes from './ProfileFollowingUsers.module.css'
 import axios from 'axios';
 import {API_URL} from '../../../shared/constants';
-//
-// const fetchFollowingUser = async () => {
-//     return await axios({
-//         method: 'get',
-//         url: `${API_URL}/users/getObservedUsers`,
-//         headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
-//     }).then(function (response) {
-//         //handle success
-//         console.log(response);
-//
-//         return response[0].username;
-//     })
-//         .catch(function (response) {
-//             //handle error
-//             console.log(response);
-//         });
-// }
+
 const ProfilefollowingUsers = () => {
     const [data, setData] = useState([]);
     useEffect(() => {
