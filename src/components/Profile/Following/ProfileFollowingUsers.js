5bd579bd725e2e582f236ac24d99b4508294db25
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-06-06 15:42:54 +0200

    Dodanie pobierania oberwowanych userów

--- src/components/Profile/Following/ProfileFollowingUsers.js
+++ src/components/Profile/Following/ProfileFollowingUsers.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import classes from './ProfileFollowingUsers.module.css'
 import axios from 'axios';
 import {API_URL} from '../../../shared/constants';
-
+//
+// const fetchFollowingUser = async () => {
+//     return await axios({
+//         method: 'get',
+//         url: `${API_URL}/users/getObservedUsers`,
+//         headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+//     }).then(function (response) {
+//         //handle success
+//         console.log(response);
+//
+//         return response[0].username;
+//     })
+//         .catch(function (response) {
+//             //handle error
+//             console.log(response);
+//         });
+// }
 const ProfilefollowingUsers = () => {
-
-    const fetchFollowingUsersList = () => {
-        axios({
-            method: 'get',
-            url: `${API_URL}/users/getObservedUsers`,
-            headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
-        }).then(function (response) {
-            //handle success
-            console.log(response);
-
-            return response[0].username;
-        })
-            .catch(function (response) {
-                //handle error
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchJson = async () => {
+            const response = await axios({
+                method: 'get',
+                url: `${API_URL}/users/getObservedUsers`,
+                headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+            }).then(function (response) {
+                //handle success
                 console.log(response);
-            });
-    }
+                return response;
+            })
+                .catch(function (response) {
+                    //handle error
+                    console.log(response);
+                });
+            console.log(response.data)
+            setData(response.data)
+        };
+        fetchJson();
+    }, []);
     return (
         <div className={classes.Following}>
             <h3>OBSERWOWANI UŻYTKOWNICY</h3>
             <div>
                 <ul>
-                    <li>{fetchFollowingUsersList}</li>
+                    {data.map(item => (
+                        <li>
+                            {item.username}
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
     )
 }
-
 export default ProfilefollowingUsers;
