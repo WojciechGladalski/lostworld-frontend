925b1dd5c1ba6bf13572c032b12b5ae995f390ea
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-06-13 12:39:28 +0200

    Dodanie reduxa do komponentu genreującego plany

--- src/components/TravelPlans/TravelPlansProfile.js
+++ src/components/TravelPlans/TravelPlansProfile.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {connect} from "react-redux";
 import classes from "./TravelPlansProfile.module.css";
+import * as actions from '../../../store/actions/index'
+import Spinner from '../../UI/Spinner/Spinner';
 
-const TravelPlans = () => (
-    <div className={classes.TravelPlans}>
-        <h3>MOJE PLANY PODRÓŻY</h3><a href="/addTravelPlan">(dodaj)</a>
-        <div>
-            <ul>
-                <li>Plan 1</li>
-                <li>Plan 2</li>
-                <li>Plan 3</li>
-            </ul>
+const TravelPlans = props => {
+    const {onFetchTravelPlans} = props;
+
+    useEffect(() => {
+        onFetchTravelPlans()
+    }, [onFetchTravelPlans]);
+
+    let plans = <Spinner/>
+
+    if (!props.loading) {
+        plans = props.travelPlans.map(plans => (
+            <li>
+                {/*DO USTALENIA Z WOJTKIEM JAK SIĘ NAZYWA TO POLE W JSONIE*/}
+                {plans.travelPlans}
+            </li>
+        ))
+    }
+    return (
+        <div className={classes.TravelPlans}>
+            <h3>MOJE PLANY PODRÓŻY</h3><a href="/addTravelPlan">(dodaj)</a>
+            <div>
+                <ul>
+                    {plans}
+                </ul>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
-export default TravelPlans;
+const mapStateToProps = state => {
+    return {
+        travelPlans: state.travelPlans.travelPlans,
+        loading: state.travelPlans.loading
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchTravelPlans: () => dispatch(actions.fetchTravelPlans())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TravelPlans);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -12,3 +12,7 @@ export {
 export {
     fetchFollowingUsers
 } from './followingUsers'
+
+export {
+    fetchTravelPlans
+} from './travelPlans'
