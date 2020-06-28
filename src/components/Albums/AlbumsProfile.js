928ce9b8dbbe14c38c8be21293f3ab64d452f1f8
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-06-28 16:03:36 +0200

    Dodanie reduxa do pobierania albumów

--- src/components/Albums/AlbumsProfile.js
+++ src/components/Albums/AlbumsProfile.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux'
 import classes from './AlbumsProfile.module.css';
+import * as actions from '../../store/actions/index';
 import AddPhotoForm from "../Forms/AddPhotoForm";
+import Spinner from '../UI/Spinner/Spinner';
 
-const AlbumsProfile = () => (
-    <div className={classes.Albums}>
-        {/*<h3>MOJE ALBUMY</h3><a href="/test">(dodaj)</a>*/}
-        <AddPhotoForm/>
-        <div></div>
-    </div>
-)
+const AlbumsProfile = props => {
 
-export default AlbumsProfile;
+    const {onFetchAlbums} = props;
+
+    useEffect(() => {
+        onFetchAlbums();
+    }, [onFetchAlbums]);
+
+    let albums = <Spinner/>
+
+    if (!props.loading) {
+        albums = props.articles.map(alb => (
+            <li>
+                {alb.albums}
+            </li>
+            )
+        )
+    }
+
+    return (
+        <div className={classes.Albums}>
+            {/*<h3>MOJE ALBUMY</h3><a href="/test">(dodaj)</a>*/}
+            <AddPhotoForm/>
+            <div>
+                <ul>
+                    {albums}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        albums: state.albums.albums,
+        loading: state.albums.loading
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchAlbums: () => dispatch(actions.fetchAlbums())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumsProfile);
