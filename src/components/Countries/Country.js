a6861068032ff6530cf9a7161b8a9590edc4af60
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-07-03 22:46:32 +0200

    Utworzenie wstępnego szablonu strony kraju

---
+++ src/components/Countries/Country.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/AuxComponent/AuxComponent'
+
+const Country = () => {
+
+    return (
+        <Aux>
+            <div>Nagłówek</div>
+            <nav>Lewy panel boczny</nav>
+            <div>Treść główna</div>
+            <nav>Prawy panel</nav>
+        </Aux>
+    )
+}
+
+export default Country;
