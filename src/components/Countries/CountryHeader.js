1213c8f7b256b033866ef2584877377c0daddc46
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-07-08 11:29:27 +0200

    Dodanie nagłówka na stronie kraju

--- src/components/Countries/CountryHeader.js
+++ src/components/Countries/CountryHeader.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import classes from './CountryHeader.module.css';
-import icelandFlag from '../../assets/images/iceland-flag.png';
-import icelandLandscape from '../../assets/images/iceland-landscape.jpg';
 
 const CountryHeader = () => (
     <div className={classes.MainContainer}>
-        <div className={classes.FlagContainer}>
-            <img className={classes.Flag} src={icelandFlag}/>
-        </div>
-        <div className={classes.LandscapeContainer}>
-            {/*<img className={classes.Landscape} src={icelandLandscape}/>*/}
-        </div>
+        <div className={classes.FlagContainer}> </div>
+        <div className={classes.LandscapeContainer}> </div>
     </div>
 
 )
