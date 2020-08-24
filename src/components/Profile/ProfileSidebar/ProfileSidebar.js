3c21d5006761331631c8db8b57c7b556f24ab7e2
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-08-24 20:36:54 +0200

    Style w profilu użytkownika

--- src/components/Profile/ProfileSidebar/ProfileSidebar.js
+++ src/components/Profile/ProfileSidebar/ProfileSidebar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import classes from "./ProfileSidebar.module.css";
+import ProfilePhoto from '../../../assets/images/gruzja.jpg'
 
 const profileSidebar = props => (
     <nav className={classes.Sidebar}>
-        <div className={classes.ProfilePhoto}>Zdjęcie profilowe</div>
-        <div className={classes.Motto}>Motto</div>
+        <div className={classes.ProfilePhoto}>
+            <img src={ProfilePhoto}/>
+        </div>
+        <div className={classes.Motto}><span>"Bez ryzyka nie ma zabawy"</span></div>
         <div className={classes.UserInfo}>Informacje o użytkowniku</div>
         <div className={classes.Friends}>Box ze znajomymi</div>
     </nav>
